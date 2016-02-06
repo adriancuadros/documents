701c99707aadd0a063355c54e1058ecfdd33c6c6
Adrian Cuadros <adrian@innku.com>
2016-02-06 11:36:37 -0600

    Document list view

--- app.rb
+++ app.rb
@@ -5,6 +5,18 @@ get '/' do
   erb :new_document
 end
 
+get '/documents' do
+  @documents = []
+  4.times do
+    @documents << Document.new(
+      'Titulo',
+      'Tag1, Tag2',
+      'Descripción'
+    )
+  end
+  erb :documents
+end
+
 post '/create_document' do 
   @document = Document.new(
     params[:title],
